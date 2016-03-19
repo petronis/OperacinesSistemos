34eda0def11e2d1a032a3802228c765a62fc8e54
Viktor <justtheviktor@gmail.com>
2016-03-19 14:48:23 +0200

    Update

--- src/com/company/RM.java
+++ src/com/company/RM.java
@@ -21,7 +21,7 @@ public class RM extends Machine {
         registers.addRegister(new Register("CH1", 1, "0"));
         registers.addRegister(new Register("CH2", 1, "0"));
         registers.addRegister(new Register("CH3", 1, "0"));
-        registers.addRegister(new Register("MODE", 1));
+        registers.addRegister(new Register("MODE", 1, "S"));
         vm = new VM(vm_blocks, this);
         external = new Memory(external_blocks);
         instructions = new Instructions(this, vm);
